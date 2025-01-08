1ddb8a54354a54a7f572c8b1af8d29f0a58a276f
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-01-08 14:19:46 -0300

    Pequenas alterações

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,8 +6,8 @@ const Formulario = () => {
         <section className="formulario">
             <form>
                 <h2>Preencha os dados para criar o card do agente</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
+                <CampoTexto label="Nome" placeholder="Digite o nome do agente" />
+                <CampoTexto label="Função" placeholder="Digite a função do agente" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
             </form>
         </section>
