ba5e25db18d35062b69537ea18d5c979716a8d0e
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-01-10 20:24:45 -0300

    Parte 4 concluída

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 import Time from "./componentes/Time";
+import Rodape from "./componentes/Rodape";
 
 function App() {
     const times = [
@@ -57,6 +58,7 @@ function App() {
                         campeao => campeao.time === time.nome)}
                 />
             )}
+            <Rodape/>
         </div>
     );
 }
